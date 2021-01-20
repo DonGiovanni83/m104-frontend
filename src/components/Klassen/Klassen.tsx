aa61c2e8566dd80b1521b53210a050d94d42f228
Fabio <flbertagna@gmail.com>
2021-01-20 19:22:26 +0100

    Add basic form for klassen

--- src/components/Klassen/Klassen.tsx
+++ src/components/Klassen/Klassen.tsx
@@ -1,60 +1,97 @@
-import React, {useEffect, useState} from "react";
-import {List} from "../List/List";
-import withLoading from "../withLoading";
-import {Button} from "react-bootstrap";
-import * as Icon from 'react-bootstrap-icons';
-import KlassenForm from "./KlassenForm";
+import React, {useState} from "react";
+import {Button, Spinner, Table} from "react-bootstrap";
 import asModalForm from "../Generics/asModalForm";
+import KlassenForm from "./KlassenForm";
+import {gql, useQuery} from "@apollo/client";
+import {Klasse} from "../../data/api";
+
+interface AlleKlassen {
+    klassen: Klasse[]
+}
+
+
+export const GET_KLASSEN = gql`
+  query GetKlassen {
+    klassen {
+      id
+      name
+      adresse{
+      ort
+      plz
+      adresse_1
+      adresse_2
+      tel_g
+      tel_m
+      email_1
+      email_2
+      }
+    }
+  }
+`;
+
 
 export default function Klassen() {
-    const LoadingComponent = withLoading(List);
+    const {loading, error, data} = useQuery<AlleKlassen>(GET_KLASSEN,);
+
     const KlassenFormModal = asModalForm(KlassenForm)
-    const [appState, setAppState] = useState({
-        loading: false,
-        columns: null,
-        classes: null,
-    });
 
     const [showModal, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-    const handleSave = () => {
-        console.log("saved")
-        setShow(false);
-    }
-
-    useEffect(() => {
-        setAppState({loading: true, columns: null, classes: null});
-        const apiUrl = process.env.REACT_APP_BACKEND_API_URL!.concat(`/klassen`);
-        console.log(apiUrl)
-        fetch(apiUrl)
-            .then((res) => res.json())
-            .then((klassen) => {
-                setAppState({loading: false, columns: klassen[0], classes: klassen.shift()});
-            });
-    }, [setAppState]);
+    const closeModal = () => setShow(false);
 
     return (
         <div>
             <div className="Site-header">
                 <h2>Klassen</h2>
                 <div>
-                    <Button variant="primary" onClick={handleShow}>
-                        <Icon.Plus/>
+                    <Button size={"lg"} variant="primary" onClick={handleShow}>
+                        Neu +
                     </Button>
                     <KlassenFormModal
-                        title={"Neue Klasse erfassen"}
-                        description={"blablabab"}
+                        title={"Neue Schule erfassen"}
+                        description={""}
                         showModal={showModal}
                         handleClose={handleClose}
-                        handleSave = {handleSave}
+                        closeModal={closeModal}
                     />
                 </div>
 
             </div>
-            <LoadingComponent loading={appState.loading} columnNames={appState.columns}
-                              entries={appState.classes}/>
+
+            <div> {loading ?
+                (
+                    <Spinner animation="border" role="status">
+                        <span className="sr-only">Wird geladen...</span>
+                    </Spinner>)
+                : (
+                    <Table>
+                        <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Schule</th>
+                            <th>Ort</th>
+                            <th>PLZ</th>
+                            <th>Adresse 1</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {data?.klassen?.map(klasse => (
+                            <tr key={klasse.id}>
+                                <td>{klasse.id}</td>
+                                <td>{klasse.name}</td>
+                                <td>{klasse.schule.adresse.ort}</td>
+                                <td>{klasse.schule.adresse.plz}</td>
+                                <td>{klasse.schule.adresse.adresse_1}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </Table>
+                )
+            }
+            </div>
         </div>
     );
 }
--- src/components/Klassen/KlassenForm.tsx
+++ src/components/Klassen/KlassenForm.tsx
@@ -1,23 +1,80 @@
-import {Form} from "react-bootstrap";
-import React from "react";
+import {Button, Col, Form} from "react-bootstrap";
+import React, {FormEvent, useState} from "react";
 import {InferProps} from "prop-types";
+import {gql, useMutation, useQuery} from "@apollo/client";
+import {GET_KLASSEN} from "./Klassen";
+
+const CREATE_KLASSE = gql`
+  mutation CreateKlasse(
+  $name: String,
+  $schule_id: ID,
+  ) {
+    create_klasse(input_args:{
+    name:$name
+    schule_id:$schule_id
+  }){
+    ok
+  }
+ }
+`;
 
 interface KlassenFormProps {
-    handleClose: (event: React.MouseEvent<HTMLElement, MouseEvent>) => void,
-    handleSave: (event: React.MouseEvent<HTMLElement, MouseEvent>) => void
+    closeModal: () => void
 }
 
-function KlassenForm({handleSave}: InferProps<KlassenFormProps>) {
+function KlassenForm({closeModal}: InferProps<KlassenFormProps>) {
+    const [createKlasse, {loading: mutationLoading, error: mutationError}] = useMutation(CREATE_KLASSE, {
+        refetchQueries: [
+            {query: GET_KLASSEN}
+        ]
+    });
+
+
+    const [formData, setFormData] = useState({
+        name: '',
+        schule_id: ''
+    });
+
+    const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({...formData, [e.target.id]: e.target.value})
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        closeModal()
+        e.preventDefault();
+        createKlasse({
+            variables: {
+                name: formData.name,
+                schule_id: formData.schule_id
+            }
+        });
+    }
     return (
-        <Form>
-            <Form.Group controlId="exampleForm.ControlInput1">
+        <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="name">
                 <Form.Label>Klassenname</Form.Label>
-                <Form.Control type="text" placeholder="z.B. INFW2020b"/>
+                <Form.Control required onChange={changeHandler} type="text" placeholder=""/>
             </Form.Group>
-            <Form.Group controlId="exampleForm.ControlInput1">
-                <Form.Label>Klasse</Form.Label>
-                <Form.Control type="text" placeholder="z.B. INFW2020b"/>
+            <Form.Group controlId="name">
+                <Form.Label>Schule</Form.Label>
+                <Form.Control required onChange={changeHandler} type="text" placeholder="" as="select">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                </Form.Control>
             </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Speichern
+            </Button>
+            {
+                mutationLoading && <p>Loading...</p>
+            }
+            {
+                mutationError && <p>Error :( Please try again</p>
+            }
         </Form>
     );
 }
